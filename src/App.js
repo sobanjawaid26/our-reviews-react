0b22aa8c1a015e921e98e4aabee98649d4bb9139
sobanjawaid26 <sobanjawaid26@gmail.com>
2024-03-15 12:42:13 +0530

    Review.js added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import React from 'react';
+import Review from './Review';
 
 function App() {
   return (
-    <div className="App">
-      Hi
-    </div>
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
+    </main>
   );
 }
 
